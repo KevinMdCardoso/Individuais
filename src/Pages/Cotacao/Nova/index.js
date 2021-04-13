292e2a4d438e79cb9e12044d334353a87ea4840f
kevin cardoso <kevinmdcardoso@gmail.com>
2021-04-12 21:01:28 -0300

    initial commit

---
+++ src/Pages/Cotacao/Nova/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react"
+import { Container, Headers } from "./style"
+
+export default class cotacao extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {}
+  }
+
+  render() {
+    return (
+      <>
+        <Headers>Multicalculo</Headers>
+        <Container>
+
+        </Container>
+      </>
+    )
+  }
+}
---
+++ src/Pages/Cotacao/Nova/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+export const Container = styled.div`
+  display: flex;
+  color: black;
+  font-weight: 400;
+`
+
+export const Headers = styled.div`
+  width: 100%;
+  background-color: black;
+  color: white;
+  height: 50px;
+  font-weight: 600;
+  font-size: 30px;
+  text-aling: center;
+`
